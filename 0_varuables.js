499b9b35ffe8c4eec2c9ce87a79302e73be08977
slaman-farcy <salmanfarcy253@gmail.com>
2024-07-19 10:32:49 +0600

    add some folder

---
+++ 0_varuables.js
@@ -0,0 +1,14 @@
+// console.log(4+6)
+
+
+// var myAge = 34;
+// console.log(myAge)
+
+// var my_firstName ="john"; //*valid
+// console.log(my_firstName)
+// var _age = 34;  //*valid
+// console.log(_age)
+// var $cityName = "new work" //*valid
+// console.log($cityName)
+// var my@Email = "salmanfarcy"
+// console.log(my@Email)//!this not valid  
---
+++ 1_datatypes.js
@@ -0,0 +1,56 @@
+
+// var myName = "salman farcy" //string data type
+// console.log(myName)
+
+// var myFavNum = 34; //namber data type
+// console.log(myFavNum)
+
+// var isRaning = true; //boolien true/false data type
+// console.log(isRaning);
+
+// var salman;
+// console.log(salman); //Undefined data type
+
+// var badMemories = null; //null data type
+// console.log(badMemories);
+
+// const bigBoy = 192371092732742349124723n; //bigent data tipe 
+// console.log(bigBoy)
+
+// const symbol = ; //!symbol data type  (pore jane nnibo)
+
+
+
+// var names = "salman";
+// console.log(typeof names) //string
+
+// var names = 45;
+// console.log(typeof names) //number
+
+// var names = true;
+// console.log(typeof names) //Boolean
+
+// var names 
+// console.log(typeof names) //undefined
+
+// var names = null;
+// console.log(typeof names) //!null is a (object) data type
+
+// var names = 459132480128403284018204n;
+// console.log(typeof names) //bigint
+
+
+
+// ? how to converd string to number
+// var age = +"20";
+// console.log( typeof age)//TODO:this is 1 way converd string to number
+
+// var age = "20";
+// console.log( typeof Number(age))//TODO:this is 2 way converd string to number
+
+// ? how to converd number to string
+// var str = 10;
+// console.log(typeof String(str));//TODO: the way of number to string convert
+
+// var str = 10;
+// console.log(typeof (str+""));//TODO: the way of number to string convert
