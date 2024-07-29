14972bba4660cc4cb07b44253a0ba8bd0bd5bad5
slaman-farcy <salmanfarcy253@gmail.com>
2024-07-29 12:32:45 +0600

    use reduce()  method in addition

--- 6_arrays.js
+++ 6_arrays.js
@@ -322,3 +322,13 @@
 //     return `Mr:${nam}`
 // })
 // console.log(result);
+
+
+//? Reduce method
+//! Write a JavaScript function that calculates the total price of items in a shopping cart. The function should take an array of item prices as imput and return total price.
+const numbers = [30.4];
+numbers.push(20.6, 30.8)
+const totalPrice = numbers.reduce((accum, curElem) => {
+    return accum + curElem
+}, 0);
+console.log(totalPrice);
