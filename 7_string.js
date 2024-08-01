5cc098a83c206bf8d0ba7609522126a3e8cd7f63
slaman-farcy <salmanfarcy253@gmail.com>
2024-08-01 08:52:21 +0600

    Array.from() lastIndexOf() search() match() matchAll()

--- 7_string.js
+++ 7_string.js
@@ -7,9 +7,43 @@
 // const str = "Hello, World!";
 // console.log(str.length);
 
-
-//? indexOf
-
+//* indexOf
 // let text = "salman farcy";
 // console.log(text.indexOf("Farcy"))
 // console.log(text.indexOf("farcy"))
+
+//* Array.from()
+// let text = "salman farcy";
+// let strArr = Array.from(text);
+// let strMap = strArr.map((element, index) => {
+//    return `${element} - ${index}`
+// })
+// console.table(strMap);
+
+//* lastIndexOf()
+// let text = "Hello JavaScript , welcome to our world best JavaScript Cours";
+// let index = text.lastIndexOf('JavaScript')
+// console.log(index);
+
+//* search()
+// let text = "Hello JavaScript , welcome to our world best JavaScript Cours";
+// let result = text.search(/javascript/i);
+// console.log(result);
+
+//* match()
+// let text = "Hello JavaScript , welcome to our world best JavaScript Cours";
+// let result = text.match(/Javascript/gi);
+// console.log(result);
+
+//* matchAll()
+// let text = "Hello JavaScript , welcome to our world best JavaScript Cours";
+// let result = text.matchAll("JavaScript");
+//   console.log(...result);
+// 
+// for (let item of result){
+//    console.log(item[0]);
+// }
+
+// for(let index of result){
+//    console.log(index.index);
+// }
