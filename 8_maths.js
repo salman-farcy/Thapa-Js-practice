777709acbf89a3ab1a11a457439208ff004a45b2
slaman-farcy <salmanfarcy253@gmail.com>
2024-08-27 11:29:30 +0600

    Math methd

--- 8_maths.js
+++ 8_maths.js
@@ -1,27 +1,27 @@
 
-const piVale = Math.PI
-console.log(piVale);
-
-console.log(Math.abs(5));
-console.log(Math.abs(-5));
-
-
-const roundedValue = Math.round(3.7)
-console.log(roundedValue);
-
-
-const celValue = Math.ceil(3.1)
-console.log(celValue);
-
-const floorValue = Math.floor(3.9)
-console.log(celValue);
-
-const truncValue = Math.trunc(3.909808)
-console.log(truncValue);
-
-
-const powValue = Math.pow(2, 5)
-console.log(truncValue);
-console.log(2 ** 5);
-
-console.log((Math.random() * 5).toFixed(3));
+// const piVale = Math.PI
+// console.log(piVale);
+// 
+// console.log(Math.abs(5));
+// console.log(Math.abs(-5));
+// 
+// 
+// const roundedValue = Math.round(3.7)
+// console.log(roundedValue);
+// 
+// 
+// const celValue = Math.ceil(3.1)
+// console.log(celValue);
+// 
+// const floorValue = Math.floor(3.9)
+// console.log(celValue);
+// 
+// const truncValue = Math.trunc(3.909808)
+// console.log(truncValue);
+// 
+// 
+// const powValue = Math.pow(2, 5)
+// console.log(truncValue);
+// console.log(2 ** 5);
+// 
+// console.log((Math.random() * 5).toFixed(3));
